195a798241f88de0345d080d6fa748e74da047f0
Bibhas <sbibhas2008@gmail.com>
2020-04-22 13:40:23 +1000

    tsp solved

--- SourceCode_and_Documentation/flask_version/routes.py
+++ SourceCode_and_Documentation/flask_version/routes.py
@@ -13,6 +13,7 @@ import urllib.request
 import urllib.parse
 import json
 from geopy import distance
+from tsp_solver.greedy import solve_tsp
 
 
 def get_distance(point1, point2):
@@ -39,6 +40,7 @@ def index(loginForm=None):
     user_landmarks = []
     planner_bool = False
     startCoords = []
+    path = []
 
     if plannerForm.validate_on_submit():
         planner_bool = True
@@ -118,68 +120,87 @@ def index(loginForm=None):
         print(lm4Coords)
 
         if lm3Coords != None and lm4Coords != None:
-            fetch_url = f"https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins={startCoords[0]},{startCoords[1]}&destinations={lm1Coords[0]},{lm1Coords[1]};{lm2Coords[0]},{lm2Coords[1]};{lm3Coords[0]},{lm3Coords[1]};{lm4Coords[0]},{lm4Coords[1]}&travelMode=driving&distanceUnit=km&key=AmiufPk0e3QV0l2SC-0A-XBgPH3rd6dCMmgyfyumfhh35u3BMjbY_4SXA70aOEtA"
+            fetch_url = f"https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins={startCoords[0]},{startCoords[1]};{lm1Coords[0]},{lm1Coords[1]};{lm2Coords[0]},{lm2Coords[1]};{lm3Coords[0]},{lm3Coords[1]};{lm4Coords[0]},{lm4Coords[1]}&destinations={startCoords[0]},{startCoords[1]};{lm1Coords[0]},{lm1Coords[1]};{lm2Coords[0]},{lm2Coords[1]};{lm3Coords[0]},{lm3Coords[1]};{lm4Coords[0]},{lm4Coords[1]}&travelMode=driving&distanceUnit=km&key=AmiufPk0e3QV0l2SC-0A-XBgPH3rd6dCMmgyfyumfhh35u3BMjbY_4SXA70aOEtA"
             user_landmarks = [
+                {
+                    'coordinate': startCoords,
+                    'name': plannerForm.start.data,
+                    'index': 1,
+                    'cost': None
+                },
                 {
                     'coordinate': lm1Coords,
                     'name': plannerForm.landmark1.data,
-                    'index': None,
+                    'index': 2,
                     'cost': None
                 },
                 {
                     'coordinate': lm2Coords,
                     'name': plannerForm.landmark2.data,
-                    'index': None,
+                    'index': 3,
                     'cost': None
                 },
                 {
                     'coordinate': lm3Coords,
                     'name': plannerForm.landmark3.data,
-                    'index': None,
+                    'index': 4,
                     'cost': None
                 },
                 {
                     'coordinate': lm4Coords,
                     'name': plannerForm.landmark4.data,
-                    'index': None,
+                    'index': 5,
                     'cost': None
                 }
             ]
         elif lm3Coords != None:
-            fetch_url = f"https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins={startCoords[0]},{startCoords[1]}&destinations={lm1Coords[0]},{lm1Coords[1]};{lm2Coords[0]},{lm2Coords[1]};{lm3Coords[0]},{lm3Coords[1]}&travelMode=driving&distanceUnit=km&key=AmiufPk0e3QV0l2SC-0A-XBgPH3rd6dCMmgyfyumfhh35u3BMjbY_4SXA70aOEtA"
+            fetch_url = f"https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins={startCoords[0]},{startCoords[1]};{lm1Coords[0]},{lm1Coords[1]};{lm2Coords[0]},{lm2Coords[1]};{lm3Coords[0]},{lm3Coords[1]}&destinations={startCoords[0]},{startCoords[1]};{lm1Coords[0]},{lm1Coords[1]};{lm2Coords[0]},{lm2Coords[1]};{lm3Coords[0]},{lm3Coords[1]}&travelMode=driving&distanceUnit=km&key=AmiufPk0e3QV0l2SC-0A-XBgPH3rd6dCMmgyfyumfhh35u3BMjbY_4SXA70aOEtA"
             user_landmarks = [
+                {
+                    'coordinate': startCoords,
+                    'name': plannerForm.start.data,
+                    'index': 1,
+                    'cost': None
+                },
                 {
                     'coordinate': lm1Coords,
                     'name': plannerForm.landmark1.data,
-                    'index': None,
+                    'index': 2,
                     'cost': None
                 },
                 {
                     'coordinate': lm2Coords,
                     'name': plannerForm.landmark2.data,
-                    'index': None,
+                    'index': 3,
                     'cost': None
                 },
                 {
                     'coordinate': lm3Coords,
                     'name': plannerForm.landmark3.data,
-                    'index': None,
+                    'index': 4,
                     'cost': None
                 }
             ]
         else:
-            fetch_url = f"https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins={startCoords[0]},{startCoords[1]}&destinations={lm1Coords[0]},{lm1Coords[1]};{lm2Coords[0]},{lm2Coords[1]}&travelMode=driving&distanceUnit=km&key=AmiufPk0e3QV0l2SC-0A-XBgPH3rd6dCMmgyfyumfhh35u3BMjbY_4SXA70aOEtA"
+            fetch_url = f"https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins={startCoords[0]},{startCoords[1]};{lm1Coords[0]},{lm1Coords[1]};{lm2Coords[0]},{lm2Coords[1]}&destinations={startCoords[0]},{startCoords[1]};{lm1Coords[0]},{lm1Coords[1]};{lm2Coords[0]},{lm2Coords[1]}&travelMode=driving&distanceUnit=km&key=AmiufPk0e3QV0l2SC-0A-XBgPH3rd6dCMmgyfyumfhh35u3BMjbY_4SXA70aOEtA"
+            print(fetch_url)
             user_landmarks = [
+                {
+                    'coordinate': startCoords,
+                    'name': plannerForm.start.data,
+                    'index': 1,
+                    'cost': None
+                },
                 {
                     'coordinate': lm1Coords,
                     'name': plannerForm.landmark1.data,
-                    'index': None,
+                    'index': 2,
                     'cost': None
                 },
                 {
                     'coordinate': lm2Coords,
                     'name': plannerForm.landmark2.data,
-                    'index': None,
+                    'index': 3,
                     'cost': None
                 }
             ]
@@ -187,37 +208,152 @@ def index(loginForm=None):
         data = response.json()
         destinations = data['resourceSets'][0]['resources'][0]['destinations']
         distance_matrix = data['resourceSets'][0]['resources'][0]['results']
+        n = len(destinations)
+        matrix = []
+        i = 0
+        j = 0
+        while i < n:
+            append_matrix = []
+            j = 0
+            while j < n:
+                append_matrix.append(0)
+                j += 1
+            i += 1
+            matrix.append(append_matrix)
 
-        i = 0
-        j = 0
-        while i < len(user_landmarks):
-            while j < len(user_landmarks):
-                if user_landmarks[i]['coordinate'][0] == destinations[j]['latitude'] and user_landmarks[i]['coordinate'][1] == destinations[j]['longitude']:
-                    user_landmarks[i]['index'] = j
-                    break
-                j += 1
-            i += 1
-        i = 0
-        j = 0
-        while i < len(user_landmarks):
-            while j < len(user_landmarks):
-                if user_landmarks[i]['index'] == distance_matrix[j]['destinationIndex']:
-                    user_landmarks[i]['cost'] = distance_matrix[j]['travelDistance']
-                    break
-                j += 1
-            i += 1
-        # sorting the data
-        i = 0
-        j = 0
-        while i < len(user_landmarks) - 1:
-            while j < len(user_landmarks) - i - 1:
-                if user_landmarks[j]['cost'] > user_landmarks[j + 1]['cost']:
-                    temp = user_landmarks[j]
-                    user_landmarks[j] = user_landmarks[j + 1]
-                    user_landmarks[j + 1] = temp
-                j += 1
-            i += 1
-        print(user_landmarks)
+        for item in distance_matrix:
+            if item['originIndex'] == 0 and item['destinationIndex'] == 0:
+                matrix[0][0] = 0
+            else:
+                matrix[int(item['originIndex'])][int(item['destinationIndex'])] = item['travelDistance']   
+
+        print(matrix)
+
+        # This code block is taken from taken from https://github.com/abhishekjiitr/tsp-python/blob/master/helper.py
+        # With some modification
+        
+        from math import isinf, sqrt, degrees, acos
+        
+        def in_subset(i, s):
+            while i > 0 and s > 0:
+                s = s >> 1
+                i -= 1
+            cond = s & 1
+            return cond
+
+        def remove(i, s):
+            x = 1
+            x = x << i
+            l = length(s)
+            l = 2 ** l - 1
+            x = x ^ l
+            return x & s
+
+        def get_path(p):
+            n = len(p[0])
+            number = 2 ** n - 2
+            prev = p[number][0]
+            path = []
+            while prev != -1:
+                path.append(prev)
+                number = remove(prev, number)
+                prev = p[number][prev]
+            reversepath = [str(path[len(path)-i-1]+1) for i in range(len(path))]
+            reversepath.append("1")
+            reversepath.insert(0, "1")
+            return reversepath
+
+        def generate_subsets(n):
+            l = []
+            for i in range(2**n):
+                l.append(i)
+            return sorted(l, key = lambda x : size(x) )
+
+        def make_graph(n):
+            a = [ [-1 for i in range(n)] for j in range(n)]
+            for i in range(n):
+                for j in range(n):
+                    rand = randint(0, n)
+                    if a[i][j] < 0:
+                        a[i][j] = rand
+                        a[j][i] = rand
+                    if i == j:
+                        a[i][i] = 0
+            return a
+        def size(int_type):
+            length = 0
+            count = 0
+            while (int_type):
+                count += (int_type & 1)
+                length += 1
+                int_type >>= 1
+            return count        
+
+        def length(int_type):
+            length = 0
+            count = 0
+            while (int_type):
+                count += (int_type & 1)
+                length += 1
+                int_type >>= 1
+            return length
+
+        def tsp(a):
+            n = len(a)
+            l = generate_subsets(n)
+            cost = [ [-1 for city in range(n)] for subset in l]
+            p = [ [-1 for city in range(n)] for subset in l]
+
+            count = 1
+            total = len(l)
+            for subset in l:
+                for dest in range(n):
+                    if not size(subset):
+                        cost[subset][dest] = a[0][dest]
+                        print (dest, subset)
+                    elif (not in_subset(0, subset)) and (not in_subset(dest, subset)) :
+                        mini = float("inf")
+                        for i in range(n):
+                            if in_subset(i, subset):
+                                modifiedSubset = remove(i, subset)
+                                val = a[i][dest] + cost[modifiedSubset][i]
+                                
+                                if val < mini:
+                                    mini = val
+                                    p[subset][dest] = i
+
+                        if not isinf(mini):
+                            cost[subset][dest] = mini
+                count += 1
+            path = get_path(p)
+            
+            Cost = cost[2**n-2][0]
+            print("Total cost for travelling with minimum route is :",Cost)
+            return path
+
+        path = tsp(matrix)
+        print(path)
+
+        # i = 0
+        # j = 0
+        # while i < len(user_landmarks):
+        #     while j < len(user_landmarks):
+        #         if user_landmarks[i]['coordinate'][0] == destinations[j]['latitude'] and user_landmarks[i]['coordinate'][1] == destinations[j]['longitude']:
+        #             user_landmarks[i]['index'] = j
+        #             break
+        #         j += 1
+        #     i += 1
+        # i = 0
+        # j = 0
+        # while i < len(user_landmarks):
+        #     while j < len(user_landmarks):
+        #         if user_landmarks[i]['index'] == distance_matrix[j]['destinationIndex']:
+        #             user_landmarks[i]['cost'] = distance_matrix[j]['travelDistance']
+        #             break
+        #         j += 1
+        #     i += 1
+        
+        # print(user_landmarks)
 
         # now get the actual linestring
 
@@ -235,7 +371,8 @@ def index(loginForm=None):
                                destinationList=destinationList,
                                trip_planner=user_landmarks,
                                planner=planner_bool,
-                               planner_origin=startCoords)
+                               planner_origin=startCoords,
+                               path = path)
 
     return render_template('index.html',
                            lon=lon,
@@ -246,7 +383,8 @@ def index(loginForm=None):
                            destinationList=destinationList,
                            trip_planner=user_landmarks,
                            planner=planner_bool,
-                           planner_origin=startCoords)
+                           planner_origin=startCoords,
+                           path = path)
 
 
 @app.route('/index/postmethod', methods=['POST'])
